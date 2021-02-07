f74cc5915fb1545b792dec0a35ffcad313e92248
Alex Reid <alex.reid@reid-projects.com>
2021-02-07 01:38:27 -0500

    fix(wiegand_reader): Fixes for CI

--- esphome/components/wiegand_reader/wiegand_reader.cpp
+++ esphome/components/wiegand_reader/wiegand_reader.cpp
@@ -11,11 +11,11 @@ void ICACHE_RAM_ATTR WiegandReader::pin_state_changed_(WiegandReader *reader) {
   reader->wiegand_.setPin1State(reader->pin_d1_->digital_read());
 }
 
-void WiegandReader::received_data_(uint8_t* data, uint8_t bits, WiegandReader *reader) {
+void WiegandReader::received_data_(uint8_t *data, uint8_t bits, WiegandReader *reader) {
   uint8_t byte_count = (bits + 7) / 8;
 
   String code = "";
-  for (int i=0; i<byte_count; i++) {
+  for (int i = 0; i < byte_count; i++) {
     code = code + String(data[i], 16);
   }
   ESP_LOGD(TAG, "Data received : %s", code.c_str());
@@ -24,7 +24,8 @@ void WiegandReader::received_data_(uint8_t* data, uint8_t bits, WiegandReader *r
     trigger->process(code);
 }
 
-void WiegandReader::received_data_error_(Wiegand::DataError error, uint8_t* raw_data, uint8_t raw_bits, const char* message) {
+void WiegandReader::received_data_error_(Wiegand::DataError error, uint8_t *raw_data, uint8_t raw_bits,
+                                         const char *message) {
   ESP_LOGE(TAG, "FAILED : %s", message);
   ESP_LOGE(TAG, "   ERROR : %s", Wiegand::DataErrorStr(error));
 
@@ -32,7 +33,7 @@ void WiegandReader::received_data_error_(Wiegand::DataError error, uint8_t* raw_
   ESP_LOGE(TAG, "   BYTE COUNT : %i", byte_count);
 
   String code = "";
-  for (int i=0; i<byte_count; i++) {
+  for (int i = 0; i < byte_count; i++) {
     code = code + String(raw_data[i], 16);
   }
   ESP_LOGE(TAG, "   DECODED : %s", code.c_str());
@@ -59,8 +60,8 @@ void WiegandReader::update() {
 }
 
 void WiegandReader::set_data_pins(GPIOPin *pin_d0, GPIOPin *pin_d1){
-    this->pin_d0_ = pin_d0;
-    this->pin_d1_ = pin_d1;
+  this->pin_d0_ = pin_d0;
+  this->pin_d1_ = pin_d1;
 }
 
 float WiegandReader::get_setup_priority() const { return setup_priority::DATA; }
@@ -72,9 +73,7 @@ void WiegandReader::dump_config() {
   LOG_UPDATE_INTERVAL(this);
 }
 
-void WiegandReaderTrigger::process(String tag) {
-  this->trigger(tag.c_str());
-}
+void WiegandReaderTrigger::process(String tag) { this->trigger(tag.c_str()); }
 
 }  // namespace wiegand_reader
 }  // namespace esphome
